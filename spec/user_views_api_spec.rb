aba9b5de37bf065be8a176ab95248ffaadb22b57
Jack Clayton <clayjs0@gmail.com>
2020-09-20 11:55:53 -0600

    Add tests

--- spec/user_views_api_spec.rb
+++ spec/user_views_api_spec.rb
@@ -7,7 +7,42 @@ RSpec.describe "user views api" do
     expect(page).to have_content(/api/i)
   end
 
+  scenario "with default language", js: true do
+    expect(page).to have_toggled("bash")
+    expect(page).not_to have_toggled("ruby")
+  end
+
+  scenario "and changes language from nav", js: true do
+    click_language "ruby"
+
+    expect(page).to have_toggled("ruby")
+    expect(page).not_to have_toggled("bash")
+  end
+
+  scenario "and changes language from select", js: true do
+    select_language "ruby"
+
+    expect(page).to have_toggled("ruby")
+    expect(page).not_to have_toggled("bash")
+  end
+
   scenario "without erb tags" do
     expect(page).not_to have_content("<%=")
+  end
+
+  private
+
+  def click_language(language)
+    find(:css, "a[data-toggle-type=\"activity\"][data-toggle-language=\"#{language}\"]").click
+  end
+
+  def have_toggled(language)
+    have_css "a[data-toggle-type=\"activity\"][data-toggle-language=\"#{language}\"].active"
+    have_css "option[value=\"#{language}\"]:checked"
+    have_css "[data-toggle-type=\"visibility\"][data-toggle-language=\"#{language}\"]"
+  end
+
+  def select_language(language)
+    find(:css, "option[value=\"#{language}\"]").click
   end
 end
